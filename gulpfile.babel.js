0914a1469fe3a45683c6f851fe73354790b57c53
ddock4you <ddock4you@gmail.com>
2019-10-19 21:50:57 +0900

    - gulp 세팅, pug 설치 및 마크업 기본 설정

---
+++ gulpfile.babel.js
@@ -0,0 +1,81 @@
+import gulp from "gulp";
+// import browserSync from "browser-sync";
+import gpug from "gulp-pug";
+import sass from "gulp-sass";
+import autoprefixer from "gulp-autoprefixer";
+import sourcemaps from "gulp-sourcemaps";
+import cleanCss from "gulp-clean-css";
+
+const browserSync =  require("browser-sync").create();
+sass.compiler = require("node-sass");
+
+
+
+const path = {
+    pug: {
+       watch: "src/template/**/*.pug",
+       src: "src/template/pages/**/*.pug"  
+    },
+    dest: "./",
+    sass: "sass/*.scss",
+    css: "css/",
+    js: "js/*.js",
+    sub: "sub/sub.html",
+    index: "index.html"
+}
+
+const pug = () => 
+    gulp
+    .src(path.pug.src)
+    .pipe(gpug({ pretty: true }))
+    .pipe(gulp.dest(path.dest));
+
+const styles = () =>
+    gulp
+    .src(path.sass)
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(cleanCss({
+        level: 0, // no optimizations
+        format: {
+            breaks: {
+                afterRuleEnds: true // put a line break after every rule
+            }
+        }
+    }))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(path.css))
+    .pipe(browserSync.stream());
+    
+    
+    const webServer = () => {
+        browserSync.init({
+            port: 7777,
+            server: {
+                baseDir: "./",
+                // index:"index.html",
+                // directory: true
+            }
+        });
+    }
+    
+    
+    const watch = () => {
+        gulp.watch(path.pug.watch, pug);
+        gulp.watch(path.js, reload);
+        gulp.watch(path.index, reload);
+        gulp.watch(path.sub, reload);
+        gulp.watch(path.sass, styles).on('change', browserSync.reload);
+    }
+
+    const reload = (done) => {
+        browserSync.reload();
+        done();
+    }
+    
+
+const live = gulp.parallel([webServer, watch]);
+const build = gulp.series([styles, pug]);
+
+export const dev = gulp.series([build, live]);
