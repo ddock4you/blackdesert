14b4eea930b727b7488c5baae3d475d0c72d8d26
ddock4you <ddock4you@gmail.com>
2019-10-20 19:56:17 +0900

    - 메인 비주얼 스크립트 조작 중

--- js/main.js
+++ js/main.js
@@ -1,31 +1,33 @@
 window.onload = () => {
+    const mainSlide = $('.main-visual__slide');
+    console.log(mainSlide);
     const mySwiper = new Swiper ('.main-visual__content', {
-        // Optional parameters
-        // direction: 'vertical',
+        speed:1000,
         loop: true,
+        // autoplay: {
+        //     delay:3000
+        // },
         navigation: {
             nextEl: '.main-visual-control.next',
             prevEl: '.main-visual-control.prev',
-          }
+        }   
     })
+    mySwiper.on('slideChangeTransitionEnd', () => {
+        let num = mySwiper.activeIndex;
+        mainSlide.removeClass('animated');
+        mainSlide.eq(num).addClass('animated');
+    });
+
 
     const gm_noteSwiper = new Swiper ('.gm-note-visual__content', {
         // Optional parameters
         // direction: 'vertical',
-        loop: true,
-        navigation: {
-            nextEl: '.main-visual-control.next',
-            prevEl: '.main-visual-control.prev',
-          }
+        loop: true
     })
 
     const sidemenuSwiper = new Swiper ('.side-menu-visual__content', {
         // Optional parameters
         // direction: 'vertical',
-        loop: true,
-        navigation: {
-            nextEl: '.main-visual-control.next',
-            prevEl: '.main-visual-control.prev',
-          }
+        loop: true
     })
 }
